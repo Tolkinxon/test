1e5b0f44ddecf55233cb5b00c9a9f6a77ea3d35c
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-04-23 21:52:45 +0500

    Add needed files

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
- 
+ const express = require("express");
+ const { config } = require("dotenv");
+ config();
+ const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+
+app.listen(PORT, "localhost", ()=>console.log(`Server running on port ${PORT}`));
