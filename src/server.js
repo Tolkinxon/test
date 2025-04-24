b55f6d08fb51a3008283a0b5f903d3a275efbc2f
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-04-24 23:20:37 +0500

    Add middlewares

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@
  const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res)=> res.send('<h1>Hello world</h1>'));
+app.post('/', (req, res) => {
+    console.log(req.body);
+    
+    res.send('ishladi')
+})
 
 
 app.listen(PORT, "localhost", ()=>console.log(`Server running on port ${PORT}`));
