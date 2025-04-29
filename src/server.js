2525a4fd56a5c885caffb15cde28897bd7594d19
Tolkinxon <krosstony639gmail.com>
2025-04-29 21:50:29 +0500

    Add helmet methods

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const { serverConfig } = require("./config.js");
 const { PORT } = serverConfig;
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended: true}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.frameguard());
+app.use(morgan('dev'))
 
-app.get('/animals', (req, res)=> res.send('there is a response for req /animals'));
+app.get('/animals', (req, res)=> {
+    res.send('there is a response for req /animals')
+});
 
 
+app.use('/animals2', (req, res, next)=>{
+    res.json({error: `Severda  xatolik kuzatildi`})
+})
 
 
 
